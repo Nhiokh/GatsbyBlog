81e68b04f42f7d40c5e9a8048f9732c1f110ccb0
Samuel Abiassi <samuel.abiassi@gmail.com>
2018-01-23 15:45:06 +0100

    Add markdown querying configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,13 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
-    'gatsby-plugin-netlify-cms'
+    'gatsby-plugin-netlify-cms',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/blog`,
+        name: "markdown-pages",
+      },
+    },
   ],
 };
