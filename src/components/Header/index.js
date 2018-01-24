028b77f90d531e3264631fdb3c8b49c7ec52fbd3
Samuel Abiassi <samuel.abiassi@gmail.com>
2018-01-25 00:03:30 +0100

    Modify Header to handle mobile version

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,76 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+import './header.scss'
+
+class Menu extends React.Component  {
+  constructor(props){
+    super(props)
+    this.state= {
+      width: 0
+    }
+  }
+
+  updateDimensions = () => {
+    this.setState({width:document.body.clientWidth})
+  }
+
+  componentDidMount(){
+    this.updateDimensions();
+    window.addEventListener("resize", this.updateDimensions);
+  }
+
+  componentWillUnmount(){
+    window.removeEventListener("resize", this.updateDimensions);
+  }
+
+  render(){
+    if (this.state.width > 700) {
+      return (
+        <ul
+          style={{
+            lineHeight: 'normal',
+            margin: '0',
+            marginLeft: 16,
+            display: 'flex',
+            alignItems: 'center',
+            listStyleType: 'none',
+            color: 'white'
+          }}
+        >
+          <li>hohoho</li>
+        </ul>
+      )
+    } else {
+      return (
+        <div
+          className="mobile-app-bar"
+          style={{
+            position: 'fixed',
+            display: 'flex',
+            justifyContent: 'center',
+            height: '60px',
+            bottom: 0,
+            left: 0,
+            width: '100%',
+            background: '#eee',
+            fontFamily: 'sans-serif'
+          }}
+        >
+          <span>About</span>
+          <span>UX</span>
+          <span>Dev</span>
+          <span>Blog</span>
+          <span>Contact</span>
+        </div>
+      )
+    }
+  }
+}
+
 const Header = () => (
   <div
+    className='header'
     style={{
       background: 'rebeccapurple',
       marginBottom: '1.45rem',
@@ -10,12 +78,13 @@ const Header = () => (
   >
     <div
       style={{
+        display: 'flex',
         margin: '0 auto',
         maxWidth: 960,
         padding: '1.45rem 1.0875rem',
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <span style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
@@ -23,9 +92,10 @@ const Header = () => (
             textDecoration: 'none',
           }}
         >
-          Gatsby
+          Place logo
         </Link>
-      </h1>
+      </span>
+      <Menu />
     </div>
   </div>
 )
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -8,7 +8,7 @@ import './index.css'
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Samuel Abiassi - Développeur Web et Designer UX/UI"
+      title="Samuel Abiassi - Web Developer and UX/UI Designer"
       meta={[
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
